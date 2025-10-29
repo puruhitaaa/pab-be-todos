a527821ab464e9580c5518d2e713a3d114199a23
baiqueee <hughdev101@gmail.com>
2025-10-30 05:22:21 +0700

    misc: generate migration

---
+++ drizzle/migrations/0000_useful_quentin_quire.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "todos" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"description" text,
+	"completed" boolean DEFAULT false,
+	"created_at" timestamp DEFAULT now()
+);
